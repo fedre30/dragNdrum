4886143369f80ba1c2a03e6f1af52f54843049c6
Federica Alfano <alfanofederica95@gmail.com>
2018-04-09 16:05:43 +0200

    php added

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
         <link rel="apple-touch-icon" href="icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="src/css/normalize.css">
+        <link rel="stylesheet" href="src/css/main.css">
     </head>
     <body>
         <!--[if lte IE 9]>
@@ -21,11 +21,11 @@
 
         <!-- Add your site or application content here -->
         <p>Hello world! This is HTML5 Boilerplate.</p>
-        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="src/js/vendor/modernizr-3.5.0.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <script>window.jQuery || document.write('<script src="src/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="src/js/plugins.js"></script>
+        <script src="src/js/main.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
---
+++ src/database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: federica
+ * Date: 09/04/2018
+ * Time: 16:03
+ */
